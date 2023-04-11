05759ccb7f45cab629749d9aad13b270df5acfb2
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:55:25 +0530

    fix: status code on access token expire

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -4,11 +4,11 @@ const { createError } = require('../error/error');
 // Verify autherization token
 const verifyToken = async (req, res, next) => {
     let token = req.headers['authorization']
-    if(!token) return next(createError(401, "You're not authenticated"))
+    if(!token) return next(createError(403, "You're not authenticated"))
 
     token = token.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return next(createError(403, "Token is invalid"))
+        if(err) return next(createError(401, "Token is invalid"))
         req.user = user;
         next()
     })
@@ -21,7 +21,7 @@ const adminToken = async (req, res, next) => {
 
     token = token.split(" ")[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return next(createError(403, "Token is invalid"))
+        if(err) return next(createError(401, "Token is invalid"))
         if(user.role == 3) return next(createError(403, "Forbidden"))
         req.user = user;
         next()
