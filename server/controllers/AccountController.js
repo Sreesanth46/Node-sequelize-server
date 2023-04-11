9042dfcdd75ab84a93d3b5cf5f131299075ce659
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:55:25 +0530

    refactor: req body destructured

--- server/controllers/AccountController.js
+++ server/controllers/AccountController.js
@@ -2,45 +2,48 @@ const db = require('../models')
 const User = db.user_master
 const Login = db.login_master
 const Company = db.company_master
-
-function generatePassword() {
-    var length = 10,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*?><",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-}
+const { generatePassword } = require('../utils/generatePassword')
 
 exports.addAccount = async (req, res, next) => {
 
     // TODO: validation and error handling
 
-    if(req.user.role > req.body.role) return res.status(403).json({message: "You cannot add an Owner"});
- 
-    let user = await User.findOne({ where: {
-        [Op.or]: [{email: req.body.email}, {accountId: req.body.email}]
-    }});
-    if(user != null) return res.status(400).json({message: "Account already exists with that email or accountId"});
+    if (req.user.role > req.body.role) return res.status(403).json({ message: "You cannot add an Owner" });
+
+    let user = await User.findOne({
+        where: {
+            [Op.or]: [{ email: req.body.email }, { accountId: req.body.email }]
+        }
+    });
+    if (user != null) return res.status(400).json({ message: "Account already exists with that email or accountId" });
 
     const generatedPassword = generatePassword()
-    console.log("<< ========= Generated password ========= >> "+generatedPassword);
     const encryptedPassword = await bcrypt.hash(generatedPassword, 10)
 
-    const t = await db.sequelize.transaction();
+    const {
+        nickName,
+        accountId,
+        department,
+        jobTitle,
+        email,
+        disallowCollaboration,
+        profileEditing
+    } = req.body
 
+    const { companyId } = req.user
+
+    const t = await db.sequelize.transaction();
     try {
         user = await User.create({
-            nickName: req.body.nickName,
-            accountId: req.body.accountId,
-            department: req.body.department,
-            jobTitle: req.body.jobTitle,
-            email: req.body.email,
-            companyId: req.user.companyId,
-            disallowCollaboration: req.body.disallowCollaboration,
-            profileEditing: req.body.profileEditing
-        }, { transaction: t});
+            nickName,
+            accountId,
+            department,
+            jobTitle,
+            email,
+            disallowCollaboration,
+            profileEditing,
+            companyId
+        }, { transaction: t });
 
         await Login.create({
             userId: user.id,
@@ -53,7 +56,7 @@ exports.addAccount = async (req, res, next) => {
 
     } catch (err) {
         await t.rollback();
-        return res.status(400).json({ message: "Something went wrong please try again"});
+        return res.status(400).json({ message: "Something went wrong please try again" });
     }
 
     // TODO: Mail service
@@ -61,30 +64,32 @@ exports.addAccount = async (req, res, next) => {
 }
 
 exports.listAll = async (req, res, next) => {
-    const user = await User.findAll({ where: { companyId: req.user.companyId}});
+    const user = await User.findAll({ where: { companyId: req.user.companyId } });
     return res.status(200).json(user)
 }
 
 exports.editAccount = async (req, res, next) => {
 
-    if(req.user.role > req.body.role) return res.status(403).json({message: "Forbidden"});
+    if (req.user.role > req.body.role) return res.status(403).json({ message: "Forbidden" });
 
     // TODO: Validation and error messages
 
-    let emailChanged =false;
+    let emailChanged = false;
     let accountIdChanaged = false;
 
-    let user = await User.findOne({ where: {
-        [Op.or]: [{email: req.body.email}, {accountId: req.body.email}]
-    }});
-    if(user != null) return res.status(400).json({message: "Account already exists with that email or accountId"});
+    let user = await User.findOne({
+        where: {
+            [Op.or]: [{ email: req.body.email }, { accountId: req.body.email }]
+        }
+    });
+    if (user != null) return res.status(400).json({ message: "Account already exists with that email or accountId" });
     // if(user.email == req.body.email) return res.status(400)
-    
-    user = await User.findOne({ where: { accountId: req.param.id }});
-    const login = await Login.findOne({ where: { userId: user.id }});
 
-    if(user.email != req.body.email) emailChanged = true;
-    if(user.accountId != req.body.accountId) accountIdChanaged = true;
+    user = await User.findOne({ where: { accountId: req.param.id } });
+    const login = await Login.findOne({ where: { userId: user.id } });
+
+    if (user.email != req.body.email) emailChanged = true;
+    if (user.accountId != req.body.accountId) accountIdChanaged = true;
 
     const t = await db.sequelize.transaction();
 
@@ -98,7 +103,6 @@ exports.editAccount = async (req, res, next) => {
             disallowCollaboration: req.body.disallowCollaboration,
             profileEditing: req.body.profileEditing
         }, {
-            // where: { id: req.params.id },
             transaction: t
         });
 
@@ -107,7 +111,6 @@ exports.editAccount = async (req, res, next) => {
             email: req.body.email,
             role: req.body.role
         }, {
-            // where: { id: login.id },
             transaction: t
         });
 
@@ -115,7 +118,7 @@ exports.editAccount = async (req, res, next) => {
 
     } catch (err) {
         await t.rollback();
-        return res.status(400).json({ message: "Something went wrong please try again"})        
+        return res.status(400).json({ message: "Something went wrong please try again" })
     }
 
     // TODO: Email service on emailChanged and accountIdChanaged
@@ -124,19 +127,19 @@ exports.editAccount = async (req, res, next) => {
 
 exports.deleteAccount = async (req, res, next) => {
 
-    if(req.user.role > req.body.role) return res.status(403).json({message: "Forbidden"});
+    if (req.user.role > req.body.role) return res.status(403).json({ message: "Forbidden" });
 
-    const user = await User.findOne({ where: { id: req.params.id }});
-    if(user == null) return res.status(400).json({message: "user not found"});
+    const user = await User.findOne({ where: { id: req.params.id } });
+    if (user == null) return res.status(400).json({ message: "user not found" });
 
-    const company = await Company.findOne({ where: { email: user.email }});
-    if(company != null) return res.status(400).json({message: "You cannot delete this user"});
+    const company = await Company.findOne({ where: { email: user.email } });
+    if (company != null) return res.status(400).json({ message: "You cannot delete this user" });
 
     const t = await db.sequelize.transaction();
 
     try {
         await user.update({
-            status: 99 
+            status: 99
         }, {
             transaction: t
         });
@@ -152,8 +155,8 @@ exports.deleteAccount = async (req, res, next) => {
 
     } catch (err) {
         await t.rollback();
-        return res.status(400).json({ message: "Something went wrong please try again"})
+        return res.status(400).json({ message: "Something went wrong please try again" })
     }
 
-    return res.status(200).json({ message: "User deleted successfully", user});
+    return res.status(200).json({ message: "User deleted successfully", user });
 }
--- server/controllers/LoginController.js
+++ server/controllers/LoginController.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const db = require('../models')
-const Login  = db.login_master
-const User  = db.user_master
+const Login = db.login_master
+const User = db.user_master
 const { Op } = require("sequelize");
 const { createError } = require('../error/error')
 
@@ -12,34 +12,36 @@ const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET || "SecretKey"
 
 exports.login = async (req, res, next) => {
 
-    let authUser = await Login.findOne({ where: {
-        [Op.or]: [{email: req.body.email}, {accountId: req.body.email}],
-        status: { [Op.ne]: 99 }
-    }}); 
+    let authUser = await Login.findOne({
+        where: {
+            [Op.or]: [{ email: req.body.email }, { accountId: req.body.email }],
+            status: { [Op.ne]: 99 }
+        }
+    });
 
-    if(authUser === null) return res.send("message: User not found").status(404)
-    
+    if (authUser === null) return res.send("message: User not found").status(404)
+
     const user = await User.findOne({ where: { email: authUser.email } });
 
     const isEqual = await bcrypt.compare(req.body.password, authUser.password)
-    if(isEqual) {
+    if (isEqual) {
         const { password, ...userWithoutPassword } = authUser.dataValues;
         await authUser.increment('loginCount')
         // TODO: update last login
-        authUser = { loginId: authUser.id, email: authUser.email, role: authUser.role, companyId: user.companyId, userId: user.id}
-        const accessToken = await jwt.sign( {...authUser}, accessTokenSecret, {expiresIn: '12h'})
-        const refreshToken = await jwt.sign( {...authUser}, refreshTokenSecret, {expiresIn: '7d'})
-        return res.status(200).header("access-Token", accessToken).json({...userWithoutPassword, accessToken, refreshToken})
+        authUser = { loginId: authUser.id, email: authUser.email, role: authUser.role, companyId: user.companyId, userId: user.id }
+        const accessToken = await jwt.sign({ ...authUser }, accessTokenSecret, { expiresIn: '12h' })
+        const refreshToken = await jwt.sign({ ...authUser }, refreshTokenSecret, { expiresIn: '7d' })
+        return res.status(200).header("access-Token", accessToken).json({ ...userWithoutPassword, accessToken, refreshToken })
     } else {
         return res.status(403).send("Forbidden")
     }
 }
 
 exports.verifyToken = async (req, res, next) => {
-    if(!req.body.accessToken) return res.status(400).send("No Token")
+    if (!req.body.accessToken) return res.status(400).send("No Token")
     const token = req.body.accessToken
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) return next(createError(401, "Token is invalid"))
+        if (err) return next(createError(401, "Token is invalid"))
         return res.status(200).json(user)
     })
 }
