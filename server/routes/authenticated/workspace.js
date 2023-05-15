9ec323e18a1725a3d077b331942d797ae3874bb4
Sreesanth46 <sreesanth46@gmail.com>
2023-05-15 17:49:00 +0530

    feat: workspace route

--- server/routes/authenticated/workspace.js
+++ server/routes/authenticated/workspace.js
@@ -1,9 +1,21 @@
 const router = require('express').Router()
 const workspaceController = require('../../controllers/WorkspaceController')
-const { verifyToken } = require('../../middleware/verifyToken')
+const { adminToken, verifyToken } = require('../../middleware/verifyToken')
 
-router.post('/',verifyToken, workspaceController.add)
+router.post('/', adminToken, workspaceController.add)
 
-router.get('/',verifyToken, workspaceController.list)
+router.get('/', verifyToken, workspaceController.list)
+
+router.get('/id/:id', adminToken, workspaceController.listByWorkspaceId)
+
+router.put('/:id', adminToken, workspaceController.edit)
+
+router.delete('/:id', adminToken, workspaceController.delete)
+
+router.post('/search', verifyToken, workspaceController.search)
+
+router.post('/items/search', verifyToken, workspaceController.searchItemsInWorkspace)
+
+router.get('/projects', adminToken, workspaceController.listWorkspaceWithProject)
 
 module.exports = router
